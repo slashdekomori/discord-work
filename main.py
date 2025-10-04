d349020a3dd783ae6139f4b14641ef4af5f74f31
xkqd <danielliubchenko@gmail.com>
2025-10-05 01:53:19 +0200

    Функционал подключения к базе данных

--- main.py
+++ main.py
@@ -15,13 +15,4 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-
 client.run(token)
