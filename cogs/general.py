85677eb431b3d1e1f718c8bc3cff758cf5844d19
xkqd <danielliubchenko@gmail.com>
2025-10-05 18:49:19 +0200

    a

--- cogs/general.py
+++ cogs/general.py
@@ -6,6 +6,7 @@ class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.db = bot.db
+
     @app_commands.command(name="profile", description="Узнать информацию о себе.")
     async def profile(self, interaction: Interaction):
         embed = discord.Embed(
@@ -13,8 +14,8 @@ class General(commands.Cog):
             description=f"Информация о пользователе {interaction.user.name}",
             color=discord.Color.blurple()
         )
-        penalty_points = self.db.get_workers(interaction.user.id)
-        embed.add_field(name="Штрафы:", value=str(penalty_points), inline=False)
+        query = await self.db.get_workers(str(interaction.user.id))
+        embed.add_field(name="Штрафы:", value=int(query[1]), inline=False)
         await interaction.response.send_message(embed=embed)
 
 async def setup(bot):
--- utils/database.py
+++ utils/database.py
@@ -2,12 +2,28 @@ from sqlalchemy import text
 from sqlalchemy.ext.asyncio import create_async_engine
 from .config import settings
 import asyncio
-
+# Таблица workers
+# discord_id: str ; index = 0
+# penalty: int ; index = 1
 
 class Database:
-    engine = create_async_engine(settings.DATABASE_URL, echo=True)
+    engine = create_async_engine(settings.DATABASE_URL)
 
-    def get_workers(self,id):
-        with self.engine.connect() as conn:
-            result = conn.execute(text("SELECT penalty FROM workers WHERE id = {id}"))
-            return result.fetchall()
+    async def add_worker_if_not_exists(self,id: str):
+        async with self.engine.connect() as conn:
+                await conn.execute(text("INSERT INTO workers (discord_id, penalty) VALUES (:id, 0)"), {"id": id})
+                await conn.commit()
+
+    async def worker_exists(self,id: str):
+            async with self.engine.connect() as conn:
+                result = await conn.execute(text("SELECT 1 FROM workers WHERE discord_id = :id"), {"id": id})
+                return result.first() is not None
+            
+        
+    async def get_workers(self,id: str):
+        async with self.engine.connect() as conn:
+            if not await self.worker_exists(id):
+                await self.add_worker_if_not_exists(id)
+            result = await conn.execute(text("SELECT * FROM workers WHERE discord_id = :id"), {"id": id})
+            return result.first()
+        
